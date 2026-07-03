922d045ca36cb9c733e9ffe20d488e69505ec503
Niemand <72ports@gmail.com>
2026-07-03 22:47:22 +0700

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar/Navbar";
 
 const geist = Geist({
   subsets: ["latin"],
